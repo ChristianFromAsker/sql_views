afd54f3fd2816d586f928f14ab5b21ad96c07e26
ChristianFromAsker <christian.kartnes@rpgroup.com>
2025-09-30 13:05:01 +0200

    updating weekly_view.sql to contain all fields needed for excel export.

--- weekly_view_v.sql
+++ weekly_view_v.sql
@@ -3,6 +3,7 @@ SELECT
     d.deal_id
 
     , d.analyst_id
+    , analyst.uw_name analyst_full_name
 
     , bc.jurisdiction		budget_continent
     , bc.jurisdiction_id	budget_continent_id
@@ -20,6 +21,8 @@ SELECT
     , br.jurisdiction_id	budget_region_id
     , d.buyer_business_name
     , blf.FirmName buyer_law_firm_1
+
+    , d.create_date
 
     , d.deal_currency
     , d.deal_info
@@ -42,19 +45,23 @@ SELECT
         d.quote_due_date, ' (' , dayname(d.quote_due_date), ') ', IFNULL(d.quote_due_time, '')
     ) nbi_deadline
     , d.nbi_prepper		nbi_prepper_id
+    , np.uw_name nbi_prepper_full_name
 
     , d.primary_uw
+    , p_uw.uw_name primary_uw_full_name
 
     , d.quote_due_date
     , d.quote_due_time
 
     , d.re_quote_info
+    , rt.risk_type_name risk_type
     , rt.risk_type_name
     , rt.risk_type_id
     , rtm.risk_type_name 	risk_type_major
     , rtm.risk_type_id 		risk_type_major_id
 
     , d.secondary_uw
+    , s_uw.uw_name second_uw_full_name
     , d.spa_law
     , IF(
     d.nbi_prepper IS NULL,
@@ -102,46 +109,46 @@ SELECT
 
 FROM deals_t d
 LEFT JOIN
-stella_common.menu_list_t ds
-ON d.deal_status_id = ds.menu_id
+    stella_common.menu_list_t ds
+    ON d.deal_status_id = ds.menu_id
 LEFT JOIN
-broker_firms_t bf
-ON d.broker_firm_id = bf.broker_firm_id
+    broker_firms_t bf
+    ON d.broker_firm_id = bf.broker_firm_id
 LEFT JOIN
-broker_persons_t broker_person
-ON d.broker_person = broker_person.ID
+    broker_persons_t broker_person
+    ON d.broker_person = broker_person.ID
 LEFT JOIN
 	law_firms_t blf
-ON d.buyer_law_firm_1_id = blf.law_firm_id
+    ON d.buyer_law_firm_1_id = blf.law_firm_id
 LEFT JOIN
-stella_common.entities_t bh
-ON d.budget_home_id = bh.entity_id
+    stella_common.entities_t bh
+    ON d.budget_home_id = bh.entity_id
 LEFT JOIN
-stella_common.jurisdictions_t br
-ON bh.budget_region_id = br.jurisdiction_id
+    stella_common.jurisdictions_t br
+    ON bh.budget_region_id = br.jurisdiction_id
 LEFT JOIN
-stella_common.jurisdictions_t bc
-ON bh.entity_continent_id__jurisdictions_t = bc.jurisdiction_id
+    stella_common.jurisdictions_t bc
+    ON bh.entity_continent_id__jurisdictions_t = bc.jurisdiction_id
 LEFT JOIN
-stella_common.underwriters_t np
-ON d.nbi_prepper = np.uw_id
+    stella_common.underwriters_t np
+    ON d.nbi_prepper = np.uw_id
 LEFT JOIN
 stella_common.underwriters_t analyst
 ON d.analyst_id = analyst.uw_id
 LEFT JOIN
-stella_common.underwriters_t p_uw
-ON d.primary_uw = p_uw.uw_id
+    stella_common.underwriters_t p_uw
+    ON d.primary_uw = p_uw.uw_id
 LEFT JOIN
-stella_common.underwriters_t s_uw
-ON d.secondary_uw = s_uw.uw_id
+    stella_common.underwriters_t s_uw
+    ON d.secondary_uw = s_uw.uw_id
 LEFT JOIN
-stella_common.risk_types_t rt
-ON d.risk_type_id = rt.risk_type_id
+    stella_common.risk_types_t rt
+    ON d.risk_type_id = rt.risk_type_id
 LEFT JOIN
-stella_common.risk_types_t rtm
-ON rt.risk_type_major_id__risk_types_t  = rtm.risk_type_id
+    stella_common.risk_types_t rtm
+    ON rt.risk_type_major_id__risk_types_t  = rtm.risk_type_id
 
 WHERE
-d.is_deleted = 0
-AND d.create_date >= (CURRENT_DATE - INTERVAL 2 YEAR)
-AND d.is_test_deal_id = 94
+    d.is_deleted = 0
+    AND d.create_date >= (CURRENT_DATE - INTERVAL 2 YEAR)
+    AND d.is_test_deal_id = 94
