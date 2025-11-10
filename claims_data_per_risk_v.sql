47423644a823dd7b1b48504e4400b438d9ca161c
ChristianFromAsker <christian.kartnes@rpgroup.com>
2025-11-10 12:29:18 +0100

    Added primary and secondary uw to claims_Data_per_risk_v.sql.

--- claims_data_per_risk_v.sql
+++ claims_data_per_risk_v.sql
@@ -34,6 +34,8 @@ SELECT
     , CAST(d.lowest_rp_attpoint / d.currency_rate_deal AS DECIMAL(18,0)) lowest_rp_attpoint_eur
 
     , pox.menu_item primary_or_xs
+    , p_uw.uw_name primary_uw_full_name
+    , d.primary_uw primary_uw_id
     , d.program_summary
 
     , c.relevant_exclusion_3
@@ -42,6 +44,9 @@ SELECT
     , FORMAT(d.retention / d.currency_rate_deal,0) AS retention_eur
     , rc. menu_item risk_consequence
     , c.risk_consequence risk_consequence_id
+
+    , d.secondary_uw second_uw_id
+    , s_uw.uw_name second_uw_full_name
     , d.spa_law
 
     , d.target_desc
@@ -56,30 +61,30 @@ SELECT
     , d.total_rp_premium_on_deal
     , d.total_rp_premium_on_deal / d.currency_rate_deal total_rp_premium_on_deal_eur
 
-, d.target_business_name
+    , d.target_business_name
 
-, FORMAT(c.internal_advisor_fees / c.fx_rate_usd,0) internal_advisor_fees_usd
-, FORMAT(d.total_rp_limit_on_deal / c.fx_rate_usd,0) AS total_rp_limit_on_deal_usd
-, FORMAT(d.retention / c.fx_rate_usd,0) AS drop_start_usd
-, FORMAT(d.drop_end / c.fx_rate_usd,0) AS drop_end_usd
-, FORMAT(c.claimed_loss / c.fx_rate_usd,0) claimed_amount_usd
-, FORMAT(c.estimated_loss / c.fx_rate_usd,0) estimated_loss_usd
-, FORMAT(c.final_loss / c.fx_rate_usd,0) final_loss_usd
-, b.business_name AS broker_firm_hr
-, layer.menu_item AS primary_or_xs_hr
-, cc1.menu_item AS claim_category_1_client_hr
-, cc2.menu_item AS claim_category_2_client_hr
-, cc3.menu_item AS claim_category_3_client_hr
-, cc4.menu_item AS claim_category_4_client_hr
-, re1.menu_item AS relevant_exclusion_1_hr
-, re2.menu_item AS relevant_exclusion_2_hr
-, rc.menu_item AS risk_consequence_hr
-, rl.item_name AS risk_likelihood_hr
+    , FORMAT(c.internal_advisor_fees / c.fx_rate_usd,0) internal_advisor_fees_usd
+    , FORMAT(d.total_rp_limit_on_deal / c.fx_rate_usd,0) AS total_rp_limit_on_deal_usd
+    , FORMAT(d.retention / c.fx_rate_usd,0) AS drop_start_usd
+    , FORMAT(d.drop_end / c.fx_rate_usd,0) AS drop_end_usd
+    , FORMAT(c.claimed_loss / c.fx_rate_usd,0) claimed_amount_usd
+    , FORMAT(c.estimated_loss / c.fx_rate_usd,0) estimated_loss_usd
+    , FORMAT(c.final_loss / c.fx_rate_usd,0) final_loss_usd
+    , b.business_name AS broker_firm_hr
+    , layer.menu_item AS primary_or_xs_hr
+    , cc1.menu_item AS claim_category_1_client_hr
+    , cc2.menu_item AS claim_category_2_client_hr
+    , cc3.menu_item AS claim_category_3_client_hr
+    , cc4.menu_item AS claim_category_4_client_hr
+    , re1.menu_item AS relevant_exclusion_1_hr
+    , re2.menu_item AS relevant_exclusion_2_hr
+    , rc.menu_item AS risk_consequence_hr
+    , rl.item_name AS risk_likelihood_hr
 
-, ch.uw_initials claim_handler_hr
-, cc.uw_initials claim_creator_hr
+    , ch.uw_initials claim_handler_hr
+    , cc.uw_initials claim_creator_hr
 
-, cs.menu_item AS claim_status_hr
+    , cs.menu_item AS claim_status_hr
 
 FROM claims_t c
 LEFT JOIN
@@ -139,6 +144,12 @@ LEFT JOIN
 LEFT JOIN
     stella_common.underwriters_t ch
     ON c.claim_handler = ch.uw_id
+LEFT JOIN
+    stella_common.underwriters_t p_uw
+    ON d.primary_uw = p_uw.uw_id
+LEFT JOIN
+    stella_common.underwriters_t s_uw
+    ON d.secondary_uw = s_uw.uw_id
 LEFT JOIN
     stella_common.underwriters_t cc
     ON c.claim_creator = cc.uw_id
