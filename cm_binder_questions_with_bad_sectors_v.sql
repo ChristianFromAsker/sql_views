b18ba275ecbb3ee5da315fd167927d120bebf1fd
ChristianFromAsker <christian.kartnes@rpgroup.com>
2025-12-09 10:48:20 +0100

    cm_binder_questions_with_bad_sectors_v.sql: Formatting

--- cm_binder_questions_with_bad_sectors_v.sql
+++ cm_binder_questions_with_bad_sectors_v.sql
@@ -19,36 +19,31 @@ bs.bad_sector_id
 , bq.binder_id binder_id__binder_questions_t
 , bq.template_question_id template_question_id__binder_questions_t
 , CASE WHEN bq.template_question_id IS NOT NULL AND bq.is_deleted = 0
-THEN 'yes'
-ELSE 'no'
+    THEN 'yes'
+    ELSE 'no'
 END template_question_on_binder
 , cdq.deal_id
 , cdq.id deal_question_id
 , cdq.answer_id
 
 FROM stella_common.cm_binder_questions_t bq
-LEFT JOIN
-	stella_common.cm_bad_sectors_t bs
+LEFT JOIN stella_common.cm_bad_sectors_t bs
 	ON bq.binder_id = bs.binder_id
-LEFT JOIN
-	stella_common.cm_template_questions_t tq
-ON bq.template_question_id = tq.id
-LEFT JOIN
-	stella_common.binders_t b
+LEFT JOIN stella_common.cm_template_questions_t tq
+    ON bq.template_question_id = tq.id
+LEFT JOIN stella_common.binders_t b
 	ON bq.binder_id = b.binder_id
-LEFT JOIN
-	stella_common.sectors_t s
-ON bs.sector_id__sectors_t = s.sector_id
-LEFT JOIN
-cm_deal_questions_t cdq
-ON bq.template_question_id = cdq.template_question_id
+LEFT JOIN stella_common.sectors_t s
+    ON bs.sector_id__sectors_t = s.sector_id
+LEFT JOIN cm_deal_questions_t cdq
+    ON bq.template_question_id = cdq.template_question_id
 WHERE
-bs.is_deleted = 0
-AND bq.is_deleted = 0
-AND b.is_deleted = 0
-AND tq.is_deleted = 0
-AND cdq.is_deleted = 0
-AND bs.is_retired = 94
-AND bq.is_retired = 94
+    bs.is_deleted = 0
+    AND bq.is_deleted = 0
+    AND b.is_deleted = 0
+    AND tq.is_deleted = 0
+    AND cdq.is_deleted = 0
+    AND bs.is_retired = 94
+    AND bq.is_retired = 94
 
 AND tq.auto_referral_type IS NOT NULL
