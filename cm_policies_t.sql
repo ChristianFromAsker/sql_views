1da4047d80f0f94ff1e31aee43224cf62c4fa88f
ChristianFromAsker <christian.kartnes@rpgroup.com>
2026-02-20 11:30:30 +0100

    cm_policies_t.sql: Added insured_main_region_id.

--- cm_policies_t.sql
+++ cm_policies_t.sql
@@ -1,6 +1,9 @@
 
 
 
+-- 20 February 2026
+-- ALTER TABLE cm_policies_t ADD insured_main_region_id INTEGER AFTER insured_registered_country_id
+
 -- 29 January 2026
 -- ALTER TABLE cm_policies_t ADD deal_id__deals_t INTEGER AFTER create_date;
 -- ALTER TABLE cm_policies_t ADD policy_id__layers_t INTEGER;
