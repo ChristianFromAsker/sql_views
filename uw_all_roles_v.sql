66afa0842abad4542b0eb8fa12a66d3e172a7dc9
ChristianFromAsker <christian.kartnes@rpgroup.com>
2026-03-29 10:14:54 +0200

    uw_all_roles_v.sql: Added budget homes and minor fixes.

--- uw_all_roles_v.sql
+++ uw_all_roles_v.sql
@@ -2,6 +2,10 @@
 CREATE OR REPLACE VIEW uw_all_roles_v AS
 SELECT
     u.uw_id,
+    bh.budget_continent_id__jurisdictions_t budget_continent_id,
+    bc.jurisdiction budget_continent_name,
+    u.budget_home_id,
+    bh.budget_home_name,
     u.uw_name,
     u.uw_initials,
     u.email,
@@ -17,10 +21,14 @@ SELECT
 FROM stella_common.underwriters_t u
 JOIN stella_common.employee_role_links_t l
   ON l.uw_id__underwriters_t = u.uw_id
-JOIN employee_roles_t r
+JOIN stella_common.employee_roles_t r
   ON r.employee_role_id = l.employee_role_id__employee_roles_t
-LEFT JOIN employee_roles_t p
+LEFT JOIN stella_common.employee_roles_t p
   ON p.employee_role_id = r.parent_employee_role_id
+LEFT JOIN stella_common.budget_homes_t bh
+    ON u.budget_home_id = bh.budget_home_id
+LEFT JOIN stella_common.jurisdictions_t bc
+    ON bh.budget_continent_id__jurisdictions_t = bc.jurisdiction_id
 WHERE u.is_deleted = 0
     AND r.is_deleted = 0
     AND l.is_deleted = 0;
