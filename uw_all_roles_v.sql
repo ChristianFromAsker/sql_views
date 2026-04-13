0d30bf5d4542c3f81a594e36d688dd20a00faea0
ChristianFromAsker <christian.kartnes@rpgroup.com>
2026-04-13 14:04:21 +0200

    uw_all_roles_v.sql: Preparing for the deal role update.

--- uw_all_roles_v.sql
+++ uw_all_roles_v.sql
@@ -6,29 +6,48 @@ SELECT
     bc.jurisdiction budget_continent_name,
     u.budget_home_id,
     bh.budget_home_name,
-    u.uw_name,
-    u.uw_initials,
+
+    u.can_change_budget_home_id,
+    cbh.menu_item can_change_budget_home,
+
     u.email,
+    u.end_date employee_end_date,
+    u.nickname employee_nickname,
+
     l.employee_role_link_id,
     l.employee_role_start_date,
     l.employee_role_end_date,
+    u.start_date employee_start_date,
+    u.user_name employee_username,
+
+    r.parent_employee_role_id parent_role_id,
+    p.employee_role_name      parent_role_name,
+
     r.employee_role_id        role_id,
     r.employee_role_name      role_name,
     r.employee_role_name_us   role_name_us,
     r.is_deleted              role_is_deleted,
-    r.parent_employee_role_id parent_role_id,
-    p.employee_role_name      parent_role_name
+
+    u.user_type_id,
+    ut.menu_item user_type_name,
+
+    u.uw_initials,
+    u.uw_name
 FROM stella_common.underwriters_t u
-JOIN stella_common.employee_role_links_t l
-  ON l.uw_id__underwriters_t = u.uw_id
-JOIN stella_common.employee_roles_t r
-  ON r.employee_role_id = l.employee_role_id__employee_roles_t
+LEFT JOIN stella_common.employee_role_links_t l
+    ON u.uw_id = l.uw_id__underwriters_t
+LEFT JOIN stella_common.employee_roles_t r
+    ON r.employee_role_id = l.employee_role_id__employee_roles_t
 LEFT JOIN stella_common.employee_roles_t p
   ON p.employee_role_id = r.parent_employee_role_id
 LEFT JOIN stella_common.budget_homes_t bh
     ON u.budget_home_id = bh.budget_home_id
 LEFT JOIN stella_common.jurisdictions_t bc
     ON bh.budget_continent_id__jurisdictions_t = bc.jurisdiction_id
+LEFT JOIN stella_common.menu_list_t ut
+    ON u.user_type_id = ut.menu_id
+LEFT JOIN stella_common.menu_list_t cbh
+    ON u.can_change_budget_home_id = cbh.menu_id
 WHERE u.is_deleted = 0
     AND r.is_deleted = 0
     AND l.is_deleted = 0;
