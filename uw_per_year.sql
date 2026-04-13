4cfebe1e4d4b6f1b8326f4ce2fd4442b42d27069
ChristianFromAsker <christian.kartnes@rpgroup.com>
2026-04-13 14:04:37 +0200

    uw_per_year.sql: Preparing for deal role update.

--- uw_per_year.sql
+++ uw_per_year.sql
@@ -4,61 +4,58 @@ Set @input_year = 2025;
 SELECT
   -- SUM(fte_portion) AS total_fte_portion
      *
-FROM (
+    FROM (
     SELECT uws.uw_id
        , uws.uw_name
        , bc.jurisdiction
 
-       , ROUND(
-        GREATEST(
-            0
-            , LEAST(
+        , ROUND(
+            GREATEST(
+                0
+                , LEAST(
                     DATEDIFF(
-                            LEAST(
-                                 COALESCE(end_date, MAKEDATE(@input_year + 1, 1) - INTERVAL 1 DAY)
-                                , MAKEDATE(@input_year + 1, 1) - INTERVAL 1 DAY
-                            )
-                        , GREATEST(start_date, MAKEDATE(@input_year, 1))
-                    ) + 1
-                , 365
-              )
-        ) / 365
-    , 2
-    ) fte_portion
+                        LEAST(
+                            COALESCE(end_date, MAKEDATE(@input_year + 1, 1) - INTERVAL 1 DAY)
+                            , MAKEDATE(@input_year + 1, 1) - INTERVAL 1 DAY
+                        ), GREATEST(start_date, MAKEDATE(@input_year, 1))
+                    ) + 1, 365
+                )
+            ) / 365, 2
+        ) fte_portion
 
-FROM
-    stella_common.underwriters_t uws
-LEFT JOIN stella_common.entities_t bh
-    ON uws.budget_home_id = bh.entity_id
-LEFT JOIN stella_common.jurisdictions_t br
-    ON bh.budget_region_id = br.jurisdiction_id
-LEFT JOIN stella_common.jurisdictions_t bc
-    ON bh.entity_continent_id__jurisdictions_t = bc.jurisdiction_id
-LEFT JOIN stella_common.menu_list_t mut
-    ON uws.user_type_id = mut.menu_id
-LEFT JOIN stella_common.menu_list_t haa
-   ON uws.has_admin_access_id = haa.menu_id
-LEFT JOIN stella_common.entities_t ei
-   ON uws.budget_home_id = ei.entity_id
-LEFT JOIN stella_common.menu_list_t nh
-    ON ei.navins_home_id = nh.menu_id
-LEFT JOIN stella_common.menu_list_t ccg
-    ON uws.can_change_general_id = ccg.menu_id
-LEFT JOIN stella_common.menu_list_t ccj
-    ON uws.can_change_jurisdictions_id = ccj.menu_id
-LEFT JOIN stella_common.menu_list_t ccu
-    ON uws.can_change_uws_id = ccu.menu_id
-LEFT JOIN stella_common.menu_list_t ccbh
-    ON uws.can_change_budget_home_id = ccbh.menu_id
-LEFT JOIN stella_common.menu_list_t is_dev
-    ON uws.is_dev_id = is_dev.menu_id
-LEFT JOIN stella_common.menu_list_t isdt
-    ON uws.is_super_duper_trusted_id = isdt.menu_id
-WHERE
-    uws.is_deleted = 0
-    AND uws.budget_home_id <> 93
-    AND uws.is_employed_id = 93
-    AND user_type_id IN (149, 150, 620)
+    FROM
+        stella_common.underwriters_t uws
+    LEFT JOIN stella_common.entities_t bh
+        ON uws.budget_home_id = bh.entity_id
+    LEFT JOIN stella_common.jurisdictions_t br
+        ON bh.budget_region_id = br.jurisdiction_id
+    LEFT JOIN stella_common.jurisdictions_t bc
+        ON bh.entity_continent_id__jurisdictions_t = bc.jurisdiction_id
+    LEFT JOIN stella_common.menu_list_t mut
+        ON uws.user_type_id = mut.menu_id
+    LEFT JOIN stella_common.menu_list_t haa
+       ON uws.has_admin_access_id = haa.menu_id
+    LEFT JOIN stella_common.entities_t ei
+       ON uws.budget_home_id = ei.entity_id
+    LEFT JOIN stella_common.menu_list_t nh
+        ON ei.navins_home_id = nh.menu_id
+    LEFT JOIN stella_common.menu_list_t ccg
+        ON uws.can_change_general_id = ccg.menu_id
+    LEFT JOIN stella_common.menu_list_t ccj
+        ON uws.can_change_jurisdictions_id = ccj.menu_id
+    LEFT JOIN stella_common.menu_list_t ccu
+        ON uws.can_change_uws_id = ccu.menu_id
+    LEFT JOIN stella_common.menu_list_t ccbh
+        ON uws.can_change_budget_home_id = ccbh.menu_id
+    LEFT JOIN stella_common.menu_list_t is_dev
+        ON uws.is_dev_id = is_dev.menu_id
+    LEFT JOIN stella_common.menu_list_t isdt
+        ON uws.is_super_duper_trusted_id = isdt.menu_id
+    WHERE
+        uws.is_deleted = 0
+        AND uws.budget_home_id <> 93
+        AND uws.is_employed_id = 93
+        AND user_type_id IN (149, 150, 620)
 ) as sub
 ORDER BY uw_name
 -- */
